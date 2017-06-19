0020a3f37ec6be425bad64f176762631b64e3470
Don Paul <donpaulpm92@gmail.com>
2017-06-19 14:54:51 +0530

    Create Share.swift

---
+++ Share.swift
@@ -0,0 +1,19 @@
+func shareAct() {
+        
+        // image to share
+        let image = UIImage(named: "Image")
+        let imageToShare = [ image! ]
+        let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        activityViewController.excludedActivityTypes = [ UIActivityType.airDrop, UIActivityType.postToFacebook ]
+        self.present(activityViewController, animated: true, completion: nil)
+        
+        // text to share
+        let text = "Please sign up and get 10% off with my code: \(DB.GET_promoCode())."
+        let textToShare = [ text ]
+        let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        activityViewController.excludedActivityTypes = [ UIActivityType.airDrop, UIActivityType.postToFacebook ]
+        self.present(activityViewController, animated: true, completion: nil)
+
+    }
